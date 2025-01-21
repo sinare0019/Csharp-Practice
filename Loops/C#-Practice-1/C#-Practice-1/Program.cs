f04f94dca178659cf15f924b862431ec4e1cafe5
Sina Rezaei <117339858+sinare0019@users.noreply.github.com>
2025-01-21 20:52:20 +0330

    add loops

---
+++ Loops/C#-Practice-1/C#-Practice-1/Program.cs
@@ -0,0 +1,30 @@
+﻿int number1 = 0;
+
+////While
+while (number1 < 5)
+{
+    Console.WriteLine("Sina");
+    number1++;
+}
+
+//do While
+number1 = 0;
+do
+{
+    Console.WriteLine("Tina");
+    number1++;
+}
+while (number1 < 5);
+
+//for
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine("Hosna");
+}
+
+//foreach
+string[] names = ["Sina", "Tina", "Hosna"];
+foreach (string item in names)
+{
+    Console.WriteLine(item);
+}
