13bc724347f9687755d4edc2cb8f12c0559f1335
Sina Rezaei <117339858+sinare0019@users.noreply.github.com>
2025-01-26 17:58:50 +0330

    add event practice

---
+++ Events/C#-Practice-1/C#-Practice-1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Practice_1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            MessageEvent += BeforMessage;
+            PrintMessage();
+            Console.ReadKey();
+        }
+
+        public delegate void DlgBeforMessage();
+        public static event DlgBeforMessage MessageEvent;
+
+        public static void PrintMessage()
+        {
+            MessageEvent.Invoke();
+            Console.WriteLine("WellCom");
+        }
+
+        public static void BeforMessage()
+        {
+            Console.WriteLine("Hi");
+        }
+    }
+}
