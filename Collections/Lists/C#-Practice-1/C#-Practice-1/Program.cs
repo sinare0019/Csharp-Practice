7b97e8477d4138db37ed6136acd4a306757cc554
Sina Rezaei <117339858+sinare0019@users.noreply.github.com>
2025-01-27 13:01:15 +0330

    add collect

---
+++ Collections/Lists/C#-Practice-1/C#-Practice-1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Practice_1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> listNames = new List<string>();
+            listNames.Add("Sina");
+            listNames.Add("Hamed");
+            listNames.Add("Saeed");
+            listNames.Add("Hosein");
+
+            listNames.Sort();
+            foreach (var item in listNames)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Collections/Stack/C#-Practice-1/C#-Practice-1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Practice_1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> names = new Stack<string>();
+            names.Push("Sina");
+            names.Push("Reza");
+            names.Push("Hamid");
+            foreach (var item in names)
+            {
+                // Last input => first outPut
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
