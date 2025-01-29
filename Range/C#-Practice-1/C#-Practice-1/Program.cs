bcafd61d548dc29cf5e98d25b555a844f13ca8e9
Sina Rezaei <117339858+sinare0019@users.noreply.github.com>
2025-01-29 19:25:22 +0330

    add range

---
+++ Range/C#-Practice-1/C#-Practice-1/Program.cs
@@ -0,0 +1,22 @@
+﻿
+int[] numbers = new int[6];
+numbers[0] = 1;
+numbers[1] = 2;
+numbers[2] = 3;
+numbers[3] = 4;
+numbers[4] = 5;
+numbers[5] = 6;
+
+var numberSelect = numbers[..4];
+foreach (var number in numberSelect)
+{
+   // 1,2,3,4
+    Console.WriteLine(number);
+}
+
+var numberSelect2 = numbers[2..5];
+foreach (var number in numberSelect2)
+{
+    // 3,4,5
+    Console.WriteLine(number);
+}
