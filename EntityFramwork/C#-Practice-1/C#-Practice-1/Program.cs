cf14f15f0c5e2ffcf943e2606080cb976e19c8ba
Sina Rezaei <117339858+sinare0019@users.noreply.github.com>
2025-02-06 00:56:13 +0330

    update entity framwork

--- EntityFramwork/C#-Practice-1/C#-Practice-1/Program.cs
+++ EntityFramwork/C#-Practice-1/C#-Practice-1/Program.cs
@@ -5,6 +5,5 @@ ContextClass context = new ContextClass();
 Teacher teacher = new Teacher() { Name= "Sina",FamilyName="Rezaei"};
 context.Teachers.Add(teacher);
 context.SaveChanges();
-var teachers = context.Teachers.FirstOrDefault();
-Console.WriteLine(teachers.Name + teachers.FamilyName);
+
 //Student student = new Student() { Name= "reza" , FamilyName="Samodi", Teacher_Id=teacher.Id};
