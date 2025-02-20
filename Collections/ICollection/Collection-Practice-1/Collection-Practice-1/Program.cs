c66508b4ff2eabd8cb65f07043877e0e2c143d8f
Sina Rezaei <117339858+sinare0019@users.noreply.github.com>
2025-02-21 00:02:14 +0330

    add Enumerable and collection

---
+++ Collections/ICollection/Collection-Practice-1/Collection-Practice-1/Program.cs
@@ -0,0 +1,15 @@
+﻿ICollection<string> names = new List<string>();
+ICollection<string> names2;
+
+names.Add("Sina");
+names.Add("Saeed");
+names.Add("K1");
+names.Add("Vahid");
+
+names.Remove("Vahid");
+
+names2 = names;
+foreach (string name in names2)
+{
+    Console.WriteLine(name);
+}
---
+++ Collections/IEnumerable/Enumerable-Practice-1/Enumerable-Practice-1/Program.cs
@@ -0,0 +1,9 @@
+﻿
+// Readonly values
+IEnumerable<string> names = new List<string>() { "Sina", "Saeed", "K1" };
+
+foreach (var name in names)
+{
+    Console.WriteLine(name);
+    Console.WriteLine(name);
+}
