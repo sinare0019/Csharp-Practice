81e23d727e62953c5b9d807050da3e6a7267d35f
Sina Rezaei <117339858+sinare0019@users.noreply.github.com>
2025-02-27 22:04:07 +0330

    add asyncs

---
+++ Asynchronous/Threads&Tasks/Tasks/Tasks/Tasks/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+             Task.Run(() =>
+            {
+                for (int i = 0; i < 400; i++)
+                {
+                    Console.WriteLine("K1");
+                }
+            }).Wait();
+
+
+            Task.Run(() =>
+            {
+                for (int i = 0; i < 400; i++)
+                {
+                    Console.WriteLine("Sina");
+                }
+            });
+
+            Task.Run(() =>
+            {
+                for (int i = 0; i < 400; i++)
+                {
+                    Console.WriteLine("Saeed");
+                }
+            });
+
+
+            Task.Run(() =>
+            {
+                for (int i = 0; i < 400; i++)
+                {
+                    Console.WriteLine("Reza");
+                }
+            });
+
+            Console.ReadKey();
+        }
+    }
+}
