d952d1a301fa3a4144fbeb9f4f8f3211499409fe
Sina Rezaei <117339858+sinare0019@users.noreply.github.com>
2025-03-17 17:57:00 +0330

    add singleton

---
+++ DesinPattern/Singleton/C#-Practice-1/C#-Practice-1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Practice_1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.log().AddLog();
+            Log.log().AddLog();
+            Console.ReadKey();
+        }
+    }
+
+    public class Log
+    {
+        private static Log _log;
+        public static Log log()
+        {
+            if( _log == null )
+                _log = new Log();
+            return _log;
+        }
+
+        public void AddLog()
+        {
+            Console.WriteLine("Save Log");
+        }
+    }
+}
